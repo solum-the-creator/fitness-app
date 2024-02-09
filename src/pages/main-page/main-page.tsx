04497d5d5106703392515ec6c774349aba231788
Andrew Kostukevich <mr.kostukevi4@mail.ru>
2024-02-09 13:20:01 +0300

    feat: add background image

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -5,14 +5,16 @@ import { Layout } from 'antd';
 import { Sidebar } from '@components/sidebar';
 import { Header } from '@components/header';
 
+import styles from './main-page.module.scss';
+
 const { Content } = Layout;
 
 export const MainPage: React.FC = () => {
     return (
-        <Layout hasSider style={{ height: '100%' }}>
+        <Layout hasSider style={{ height: '100%' }} className={styles.image_container}>
             <Sidebar />
 
-            <Layout>
+            <Layout className={styles.main_container}>
                 <Header />
                 <Content>Main content</Content>
             </Layout>
