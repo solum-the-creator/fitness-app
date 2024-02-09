fef0be9a6100d6e1d7b894fb62fbd1194954f34d
Andrew Kostukevich <mr.kostukevi4@mail.ru>
2024-02-09 11:01:52 +0300

    feat: create a basic page layout

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,5 +1,24 @@
+import 'antd/dist/antd.css';
 import React from 'react';
 
+import { Layout } from 'antd';
+import { Sidebar } from '@components/sidebar';
+import { Header } from '@components/header';
+
+const { Content, Footer } = Layout;
+
 export const MainPage: React.FC = () => {
-    return <></>;
+    return (
+        <Layout hasSider style={{ height: '100%' }}>
+            <Sidebar />
+
+            <Layout>
+                <Header />
+                <Layout>
+                    <Content>Main content</Content>
+                    <Footer>Footer</Footer>
+                </Layout>
+            </Layout>
+        </Layout>
+    );
 };
