793f9cb7357c8516dfbba17bfd1e8395d44ec954
Andrew Kostukevich <mr.kostukevi4@mail.ru>
2024-02-17 14:37:09 +0300

    feat: create Layout for auth and register forms

--- src/pages/auth-page/layout-auth.tsx
+++ src/pages/auth-page/layout-auth.tsx
@@ -1,13 +1,22 @@
+import { Layout } from 'antd';
 import styles from './layout-auth.module.scss';
 
 import { Content } from 'antd/lib/layout/layout';
 import { Outlet } from 'react-router-dom';
+import { Logo } from './_components/logo';
 
 export const LayoutAuth = () => {
     return (
         <div className={styles.background}>
-            <Content>
-                <Outlet />
+            <Content className={styles.content}>
+                <Layout className={styles.form_container}>
+                    <div className={styles.form_enter}>
+                        <div className={styles.main_container}>
+                            <Logo />
+                            <Outlet />
+                        </div>
+                    </div>
+                </Layout>
             </Content>
         </div>
     );
