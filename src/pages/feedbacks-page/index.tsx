7361905c278a05262664ea4445f390fe1bd37d2b
Andrew Kostukevich <mr.kostukevi4@mail.ru>
2024-02-29 17:24:51 +0300

    feat: add hiding and unhiding the list of reviews

--- src/pages/feedbacks-page/index.tsx
+++ src/pages/feedbacks-page/index.tsx
@@ -9,6 +9,7 @@ import { useMediaQuery } from 'react-responsive';
 import { useGetFeedbackQuery } from '@redux/api/apiSlice';
 import { useLoaderLoading } from '@hooks/use-loader-loading';
 import { FeedbackCard } from './feedback-card';
+import { useEffect, useMemo, useRef, useState } from 'react';
 
 export const FeedbacksPage = () => {
     // TODO: попробовать suspense или lazy loading
@@ -17,62 +18,90 @@ export const FeedbacksPage = () => {
     const isSiderCollapsed = useSelector((state: RootState) => state.sider.isCollapsed);
     const extraPadding = isSiderCollapsed ? styles.extra_padding : styles.no_extra_padding;
 
-    const { data: feedbacks, isFetching } = useGetFeedbackQuery();
+    const { data: feedbacks = [], isFetching } = useGetFeedbackQuery();
     useLoaderLoading(isFetching);
 
+    const containerRef = useRef<HTMLDivElement>(null);
     const isEmpty = feedbacks?.length === 0;
 
+    const [showAll, setShowAll] = useState(false);
+
+    const sortedFeedbacks = useMemo(() => {
+        return [...feedbacks];
+        // TODO: add sorting by date
+        // return [...feedbacks].sort((a, b) => b.createdAt.localeCompare(a.createdAt));
+    }, [feedbacks]);
+
     console.log(feedbacks);
+
+    const limitedFeedbacks = useMemo(() => {
+        if (!showAll) {
+            return sortedFeedbacks.slice(sortedFeedbacks.length - 4);
+        }
+        return sortedFeedbacks;
+    }, [showAll, sortedFeedbacks]);
+
+    const onShowAll = () => {
+        setShowAll(!showAll);
+    };
+
+    useEffect(() => {
+        if (showAll) {
+            if (containerRef.current) {
+                containerRef.current.scrollTop = containerRef.current.scrollHeight;
+            }
+        }
+    }, [limitedFeedbacks, showAll]);
 
     return (
         <Layout className={styles.main_container}>
             <PlainHeader breadCrumbs={['Главная', 'Отзывы пользователей']} />
             <Content className={styles.content}>
-                {feedbacks &&
-                    (isEmpty ? (
-                        <div className={styles.empty_container}>
-                            <div className={`${styles.keep_feedback_container} ${extraPadding}`}>
-                                <div className={styles.keep_feedback}>
-                                    <h3 className={styles.title}>Оставьте свой отзыв первым</h3>
-                                    <p className={styles.description}>
-                                        Вы можете быть первым, кто оставит отзыв об этом фитнесс
-                                        приложении.
-                                        <br />
-                                        Поделитесь своим мнением и опытом с другими пользователями,
-                                        <br />и помогите им сделать правильный выбор.
-                                    </p>
-                                </div>
+                {isEmpty ? (
+                    <div className={styles.empty_container}>
+                        <div className={`${styles.keep_feedback_container} ${extraPadding}`}>
+                            <div className={styles.keep_feedback}>
+                                <h3 className={styles.title}>Оставьте свой отзыв первым</h3>
+                                <p className={styles.description}>
+                                    Вы можете быть первым, кто оставит отзыв об этом фитнесс
+                                    приложении.
+                                    <br />
+                                    Поделитесь своим мнением и опытом с другими пользователями,
+                                    <br />и помогите им сделать правильный выбор.
+                                </p>
                             </div>
-                            <Button
-                                type='primary'
-                                size='large'
-                                className={styles.button}
-                                block={matches}
-                            >
+                        </div>
+                        <Button
+                            type='primary'
+                            size='large'
+                            className={styles.button}
+                            block={matches}
+                        >
+                            Написать отзыв
+                        </Button>
+                    </div>
+                ) : (
+                    <div className={styles.content_container}>
+                        <div className={styles.feedbacks_container} ref={containerRef}>
+                            {limitedFeedbacks.map((feedback) => (
+                                <FeedbackCard {...feedback} key={feedback.id} />
+                            ))}
+                        </div>
+                        <div className={styles.button_container}>
+                            <Button type='primary' size='large' className={styles.feedback_button}>
                                 Написать отзыв
                             </Button>
+                            <Button
+                                type='link'
+                                size='large'
+                                className={styles.show_button}
+                                onClick={onShowAll}
+                            >
+                                {showAll ? 'Свернуть все отзывы' : 'Развернуть все отзывы'}
+                            </Button>
                         </div>
-                    ) : (
-                        <div className={styles.content_container}>
-                            <div className={styles.feedbacks_container}>
-                                {feedbacks.map((feedback) => (
-                                    <FeedbackCard {...feedback} key={feedback.id} />
-                                ))}
-                            </div>
-                            <div className={styles.button_container}>
-                                <Button
-                                    type='primary'
-                                    size='large'
-                                    className={styles.feedback_button}
-                                >
-                                    Написать отзыв
-                                </Button>
-                                <Button type='link' size='large' className={styles.show_button}>
-                                    Развернуть все отзывы
-                                </Button>
-                            </div>
-                        </div>
-                    ))}
+                    </div>
+                )}
             </Content>
         </Layout>
     );
