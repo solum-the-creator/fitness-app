97f9fa6d195bcb4d4df7d4a01550ea113a4165a2
Andrew Kostukevich <mr.kostukevi4@mail.ru>
2024-04-01 08:51:18 +0300

    feat: create base layout of the Trainings page

--- src/pages/trainings-page/index.tsx
+++ src/pages/trainings-page/index.tsx
@@ -1,6 +1,9 @@
+import { BaseHeader } from '@components/header/base-header';
 import { ErrorTrainingList } from '@components/modals/error-training-list';
+import PATHS from '@constants/paths';
 import { useLoaderLoading } from '@hooks/use-loader-loading';
 import { useGetTrainingListQuery, useGetTrainingQuery } from '@redux/api/api-slice';
+import { Content } from 'antd/lib/layout/layout';
 
 import styles from './trainings-page.module.scss';
 
@@ -17,8 +20,18 @@ export const TrainingsPage = () => {
     useLoaderLoading(isFetching || isFetchingTrainingList);
 
     return (
-        <div className={styles.container}>
-            <h1 className={styles.title}>Тренировки</h1>
+        <div className={styles.main_container}>
+            <div className={styles.header_wrapper}>
+                <BaseHeader
+                    breadCrumbs={[
+                        { title: 'Главная', link: PATHS.MAIN },
+                        { title: 'Тренировки', link: PATHS.TRAININGS },
+                    ]}
+                />
+            </div>
+            <div className={styles.content_wrapper}>
+                <Content className={styles.content}>Мои тренировки</Content>
+            </div>
             <ErrorTrainingList isError={isError} refetch={refetch} />
         </div>
     );
